c8bd129eececbd8d9025eadbec832d68e845c009
Badacadabra <baptiste.vannesson@gmail.com>
2017-09-24 18:45:32 +0200

    Android - Add Facebook Ads

--- android/App.js
+++ android/App.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, View, Alert, Share, Vibration } from 'react-native';
 import { Header } from 'react-native-elements';
-import { Font } from 'expo';
+import { Font, WebBrowser, FacebookAds } from 'expo';
 import Wheel from './src/components/Wheel';
 import Pipo from './src/components/Pipo';
 import ButtonGroup from './src/components/ButtonGroup';
@@ -20,7 +20,12 @@ type State = {
   fontLoaded: boolean
 };
 
-let sentence: string = '';
+let sentence: string = '',
+    counterJunior: number = 0,
+    counterExperienced: number = 0,
+    counterSenior: number = 0;
+
+FacebookAds.AdSettings.addTestDevice(FacebookAds.AdSettings.currentDeviceHash);
 
 export default class App extends Component<void, void, State> {
   state: State = {
@@ -48,16 +53,19 @@ export default class App extends Component<void, void, State> {
         levelColor = '#F4BF31';
         wheelSpeed = 2000;
         sky = blue;
+        counterJunior++;
         break;
       case 'confirmé':
         levelColor = '#44DBBD';
         wheelSpeed = 1000;
         sky = cloudy;
+        counterExperienced++;
         break;
       case 'senior':
         levelColor = '#9FD7FC';
         wheelSpeed = 500;
         sky = gray;
+        counterSenior++;
         break;
       default:
         throw new Error('Incorrect level!');
@@ -71,23 +79,45 @@ export default class App extends Component<void, void, State> {
     }));
   }
 
+  displayAd(): void {
+    FacebookAds.InterstitialAdManager.showAd('283520152134431_283525962133850')
+      .then(didClick => {})
+      .catch(error => {})
+  }
+
   update(bullshit: string): void {
     sentence = bullshit;
+
+    if (counterJunior === 10) {
+      counterJunior = 0;
+      this.displayAd();
+    }
+
+    if (counterExperienced === 6) {
+      counterExperienced = 0;
+      this.displayAd();
+    }
+
+    if (counterSenior === 3) {
+      counterSenior = 0;
+      this.displayAd();
+    }
   }
 
   help(): void {
     Alert.alert(
       'Brassez du vent !',
-      `Pipotronizer est un générateur de phrases corporate aussi inutiles qu'indispensables.
+      `3 niveaux de brassage éolien sont à votre disposition pour faire face à toutes les situtations.
 
-3 niveaux de brassage éolien sont à votre disposition pour faire face à toutes les situations.
+Mais attention : la force du vent détermine la fréquence des pubs, dont les revenus serviront à lutter contre la précarité énergétique.
 
-Avec Pipotronizer, la force du vent est entre vos mains !`
+Avec Pipotronizer, brasser du vent n'a jamais été aussi utile !`
     );
   }
 
-  vibrate(): void {
+  browse(): void {
     Vibration.vibrate();
+    WebBrowser.openBrowserAsync('http://pipo.badacadabra.net');
   }
 
   share(): void {
@@ -108,7 +138,7 @@ Avec Pipotronizer, la force du vent est entre vos mains !`
             <Header
               statusBarProps={{ barStyle: 'light-content' }}
               leftComponent={{ icon: 'help-outline', color: '#FFF', underlayColor: '#121212', onPress: this.help }}
-              centerComponent={{ text: 'Pipotronizer', style: styles.toolbarTitle, onPress: this.vibrate }}
+              centerComponent={{ text: 'Pipotronizer', style: styles.toolbarTitle, onPress: this.browse}}
               rightComponent={{ icon: 'share', color: '#FFF', underlayColor: '#121212', onPress: this.share }}
               outerContainerStyles={{ backgroundColor: '#181818' }}
             />
