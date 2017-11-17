9d57474e5902f5778f476c3617dcca2e70895d74
Badacadabra <baptiste.vannesson@gmail.com>
2017-11-17 19:03:56 +0100

    Android - Fix opacity issue

--- android/src/components/ButtonGroup.js
+++ android/src/components/ButtonGroup.js
@@ -60,6 +60,7 @@ const styles = StyleSheet.create({
     paddingBottom: 15,
     flexDirection: 'row',
     justifyContent: 'space-around',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF'
   }
 });
